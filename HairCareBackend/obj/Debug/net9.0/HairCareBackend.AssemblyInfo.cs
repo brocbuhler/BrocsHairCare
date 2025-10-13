d4bdc48730c75cfb975d0418dedec28cd763f130
Broc Buhler <brocbuhler@gmail.com>
2025-10-13 14:07:55 -0700

    Please

--- HairCareBackend/obj/Debug/net9.0/HairCareBackend.AssemblyInfo.cs
+++ HairCareBackend/obj/Debug/net9.0/HairCareBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HairCareBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a30ed2f5e0e59f07c03aada620d14b96cdfef58b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14679bfd56100f5f53b07e701e39dda74374fa5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HairCareBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HairCareBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
