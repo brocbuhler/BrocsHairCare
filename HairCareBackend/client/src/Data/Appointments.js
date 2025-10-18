873510b9d3d9d8ed8c239776bb680d9f2d0de2b1
Broc Buhler <brocbuhler@gmail.com>
2025-10-18 08:33:50 -0700

    accidently worked on main

---
+++ HairCareBackend/client/src/Data/Appointments.js
@@ -0,0 +1,31 @@
+const _apiUrl = "/api/appointments";
+
+const getAppointment = (id) => {
+  return fetch(`${_apiUrl}/${id}`).then((r) => r.json());
+};
+
+const createAppointment = (appointment) => {
+  return fetch(_apiUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(appointment),
+  }).then((res) => res.json());
+};
+
+const patchAppointment = (id, appointment) => {
+  return fetch(`${_apiUrl}/${id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(appointment),
+  }).then((res) => res.json());
+};
+
+const deleteAppointment = (id) => {
+  const result = fetch(`${_apiUrl}/${id}`, {
+    method: "DELETE",
+  });
+  if (result.status === 204) return null;
+  return result.json();
+};
+
+export {getAppointment, patchAppointment, createAppointment, deleteAppointment}
